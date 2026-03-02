374e7989e7c87a3079e83ffdc44ad6df14ca720b
mr-mayu-mane <mayurmane2577@gmail.com>
2026-03-02 18:57:41 +0530

    Added the condition to verify if the inpur dir is valid

--- script-practise/count_file_in_dir.py
+++ script-practise/count_file_in_dir.py
@@ -3,7 +3,7 @@ import os
 dir = input("Enter the dir: ")
 
 if os.path.isdir(dir):
-
     file = [ f for f in os.listdir(dir) if os.path.isfile(os.path.join(dir,f)) and f.endswith(".log")]
-
-print("Files in ", dir, " : ", file)
+    print("Files in ", dir, ":", file)
+else:
+    print("Invalid dir:", dir)
