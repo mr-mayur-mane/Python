185e0f4901935c31ad0bcfc11f59b43962fc7ebd
mr-mayu-mane <mayurmane2577@gmail.com>
2026-03-02 18:53:44 +0530

    Print the file in dir passed by user

--- script-practise/count_file_in_dir.py
+++ script-practise/count_file_in_dir.py
@@ -2,6 +2,8 @@ import os
 
 dir = input("Enter the dir: ")
 
-file = [ f for f in os.listdir(dir) ]
+if os.path.isdir(dir):
+
+    file = [ f for f in os.listdir(dir) if os.path.isfile(os.path.join(dir,f)) and f.endswith(".log")]
 
 print("Files in ", dir, " : ", file)
