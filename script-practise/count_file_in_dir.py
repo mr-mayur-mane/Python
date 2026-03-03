a1e8e4008814b8b0945651bc54d9e852bd03c004
mr-mayu-mane <mayurmane2577@gmail.com>
2026-03-03 20:23:32 +0530

    Count the number of file in particular format in directory

--- script-practise/count_file_in_dir.py
+++ script-practise/count_file_in_dir.py
@@ -5,5 +5,6 @@ dir = input("Enter the dir: ")
 if os.path.isdir(dir):
     file = [ f for f in os.listdir(dir) if os.path.isfile(os.path.join(dir,f)) and f.endswith(".log")]
     print("Files in ", dir, ":", file)
+    print("File count ending with .log: ", len(file))
 else:
     print("Invalid dir:", dir)
