74ab984fecc37a34650991fd1ecc1ce8ede3b8fb
mr-mayu-mane <mayurmane2577@gmail.com>
2026-03-04 19:03:27 +0530

    Print the line containing error

--- script-practise/print_string_in_file.py
+++ script-practise/print_string_in_file.py
@@ -1,6 +1,9 @@
 
 file_name = input("Enter the file path: ")
 
-with open(file_name, 'r') as f:
-    for line in f:
-        print(line)
+with open(file_name, 'r') as file:
+    for line in file:
+        if 'error' in line.lower():
+            print("Error found: ", line.strip())
+        else:
+            print("No error found")
