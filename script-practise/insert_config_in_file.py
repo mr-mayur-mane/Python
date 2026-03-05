7fc50e1e2b2aaf25d511c373d115fdbb2f8e63d9
mr-mayu-mane <mayurmane2577@gmail.com>
2026-03-05 19:03:24 +0530

    Upadte the value if present

--- script-practise/insert_config_in_file.py
+++ script-practise/insert_config_in_file.py
@@ -1,9 +1,22 @@
-file = "app.conf"
-config = "This is the new config"
+# file = "app.conf"
+# config = "This is the new config"
 
-with open(file, 'r') as f:
-    content = f.read()
-    if config not in content:
-        with open(file, 'a') as f:
-            f.write("\n" + config)
-            
+# with open(file, 'r') as f:
+#     content = f.read()
+#     if config not in content:
+#         with open(file, 'a') as f:
+#             f.write("\n" + config)
+
+
+# update the config value if present else add the config
+
+def add_config(file,key,value):
+    with open(file,'r') as f:
+        lines = f.readlines()
+    with open(file, 'w') as f:
+        for line in lines:
+            if line.strip().startswith(f"{key}"):
+                f.write(f"{key} = {value}\n")
+            else:
+                f.write(line)
+add_config("app.conf","max_connection","500")
