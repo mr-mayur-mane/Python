05fb5325ce9e36e057c9e61737af80df8112caf6
mr-mayu-mane <mayurmane2577@gmail.com>
2026-03-05 18:18:00 +0530

    Add the config to the file

---
+++ script-practise/insert_config_in_file.py
@@ -0,0 +1,8 @@
+file = "app.conf"
+config = "This is the new config"
+
+with open(file, 'r') as f:
+    content = f.read()
+    if config not in content:
+        with open(file, 'a') as f:
+            f.write("\n" + config)
