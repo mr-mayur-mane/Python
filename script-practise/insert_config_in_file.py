cdcc0226297e67c3a2e52f021e664630d283d4a8
mr-mayu-mane <mayurmane2577@gmail.com>
2026-03-05 19:07:44 +0530

    Append the config not found, if found update the value

--- script-practise/insert_config_in_file.py
+++ script-practise/insert_config_in_file.py
@@ -11,12 +11,17 @@
 # update the config value if present else add the config
 
 def add_config(file,key,value):
+    found = False
     with open(file,'r') as f:
         lines = f.readlines()
     with open(file, 'w') as f:
         for line in lines:
             if line.strip().startswith(f"{key}"):
                 f.write(f"{key} = {value}\n")
+                found = True
             else:
                 f.write(line)
-add_config("app.conf","max_connection","500")
+    if not found:
+        with open(file, 'a') as f:
+            f.write(f"{key} = {value}")
+add_config("app.conf","max_buffer","500")
