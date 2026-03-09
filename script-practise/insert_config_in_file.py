05ec21e203d238a320e610443e3624634b8c134e
mr-mayu-mane <mayurmane2577@gmail.com>
2026-03-09 19:10:40 +0530

    Update the key value if not in config

--- script-practise/insert_config_in_file.py
+++ script-practise/insert_config_in_file.py
@@ -1,11 +1,31 @@
 file = "app.conf"
-config = "node_env = prod"
 
-with open(file, 'r') as f:
-    content = f.read()
-    if config not in content:
-        print(f'No config found: {config}')
-        print(f'Adding the config...')
+key = input("Enter the config key: ")
+value = input("Enter the config value: ")
+
+# with open(file, 'r') as f:
+#     content = f.read()
+#     if config not in content:
+#         print(f'No config found: {config}')
+#         print(f'Adding the config...')
+#         with open(file, 'a') as f:
+#             f.write("\n" + config)
+
+def add_config(file, key, value):
+    found = False
+    with open(file, 'r') as f:
+        lines = f.readlines()
+    with open(file, 'w') as f:
+        for line in lines:
+            if line.strip().startswith(f'{key}'):
+                print(f'Updating the value of {key} with {value}')
+                f.write(f'{key} = {value}\n')
+                found = True
+            else:
+                f.write(f'{line}')
+                    
+    if not found:
         with open(file, 'a') as f:
-            f.write("\n" + config)
+            f.write(f'{key} = {value}\n')
 
+add_config(file, key, value)
