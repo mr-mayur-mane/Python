e3df0a598a70139701b2af22fc07c27c41d79212
mr-mayu-mane <mayurmane2577@gmail.com>
2026-03-09 18:51:05 +0530

    Add the new config if not found

--- script-practise/insert_config_in_file.py
+++ script-practise/insert_config_in_file.py
@@ -1,9 +1,11 @@
 file = "app.conf"
 config = "node_env = prod"
 
-with open(file, 'r') as file:
-    content = file.read()
+with open(file, 'r') as f:
+    content = f.read()
     if config not in content:
         print(f'No config found: {config}')
         print(f'Adding the config...')
-        
+        with open(file, 'a') as f:
+            f.write("\n" + config)
+
