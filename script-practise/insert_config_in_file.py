56bfc88774ba9efbb3c891874005ee69efe2f7ea
mr-mayu-mane <mayurmane2577@gmail.com>
2026-03-09 18:46:52 +0530

    Print the message before the config is added

--- script-practise/insert_config_in_file.py
+++ script-practise/insert_config_in_file.py
@@ -1,27 +1,9 @@
-# file = "app.conf"
-# config = "This is the new config"
+file = "app.conf"
+config = "node_env = prod"
 
-# with open(file, 'r') as f:
-#     content = f.read()
-#     if config not in content:
-#         with open(file, 'a') as f:
-#             f.write("\n" + config)
-
-
-# update the config value if present else add the config
-
-def add_config(file,key,value):
-    found = False
-    with open(file,'r') as f:
-        lines = f.readlines()
-    with open(file, 'w') as f:
-        for line in lines:
-            if line.strip().startswith(f"{key}"):
-                f.write(f"{key} = {value}\n")
-                found = True
-            else:
-                f.write(line)
-    if not found:
-        with open(file, 'a') as f:
-            f.write(f"{key} = {value}\n")
-add_config("app.conf","innode","500")
+with open(file, 'r') as file:
+    content = file.read()
+    if config not in content:
+        print(f'No config found: {config}')
+        print(f'Adding the config...')
+        
