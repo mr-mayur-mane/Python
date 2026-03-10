45769b65ba3db2dc71bec55379db79772860e7a4
mr-mayu-mane <mayurmane2577@gmail.com>
2026-03-10 15:54:14 +0530

    UPdate the value if config key found, else append the config

--- script-practise/insert_config_in_file.py
+++ script-practise/insert_config_in_file.py
@@ -1,12 +1,23 @@
 file = input("Enter the file name: ")
 
-config = "node_env = prod"
+key = input("Enter the config key: ")
 
-with open(file, 'r')as f:
-    content= f.read()
-    if config not in content:
-        print("No config found")
-        print(f'Adding config: {config}')
-        with open(file, 'a') as f:
-            f.write(f'\n{config}')
-            print("Config added.")
+value= input("Enter the config value: ")
+def add_config(file, key, value):
+    found = False
+    with open(file, 'r') as f:
+        lines = f.readlines()
+        for line in lines:
+            with open(file, 'w') as f:
+                if line.strip().startswith(f'{key}'):
+                    f.write(f'{key} = {value}')
+                    found = True
+                    print("Config key found, updating the value")
+                else:
+                    f.write(f'{line}')
+        if not found:
+            print("No config found, appending the config")
+            with open(file, 'a') as f:
+                f.write(f'\n{key} = {value}')
+
+add_config(file, key, value)
