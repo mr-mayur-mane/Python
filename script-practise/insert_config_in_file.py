20390f9f3a9cfbb2a0325683516f98f3eb17d2de
mr-mayu-mane <mayurmane2577@gmail.com>
2026-03-10 15:40:28 +0530

    Added the new config not found

--- script-practise/insert_config_in_file.py
+++ script-practise/insert_config_in_file.py
@@ -1,32 +1,12 @@
-file = "app.conf"
+file = input("Enter the file name: ")
 
-key = input("Enter the config key: ")
-value = input("Enter the config value: ")
+config = "node_env = prod"
 
-# with open(file, 'r') as f:
-#     content = f.read()
-#     if config not in content:
-#         print(f'No config found: {config}')
-#         print(f'Adding the config...')
-#         with open(file, 'a') as f:
-#             f.write("\n" + config)
-
-def add_config(file, key, value):
-    found = False
-    with open(file, 'r') as f:
-        lines = f.readlines()
-    with open(file, 'w') as f:
-        for line in lines:
-            if line.strip().startswith(f'{key}'):
-                print(f'Updating the value of {key} with {value}')
-                f.write(f'{key} = {value}\n')
-                found = True
-            else:
-                f.write(f'{line}')
-                    
-    if not found:
-        print(f'No config found.\nAdding the config: {key} = {value}')
+with open(file, 'r')as f:
+    content= f.read()
+    if config not in content:
+        print("No config found")
+        print(f'Adding config: {config}')
         with open(file, 'a') as f:
-            f.write(f'{key} = {value}\n')
-
-add_config(file, key, value)
+            f.write(f'\n{config}')
+            print("Config added.")
