eb7e0a8b55811253603c0798c0a28b1dfba30375
mr-mayu-mane <mayurmane2577@gmail.com>
2026-03-12 18:20:59 +0530

    Update the config in the file, if key found, else add the key and name

--- script-practise/insert_config_in_file.py
+++ script-practise/insert_config_in_file.py
@@ -1,23 +1,23 @@
 file = "app.conf"
 
 key = input("Enter the config key name: ")
-value = input("Enter the config value name: ")
-def app_config(file,key,value):
-    found = False
-    with open(file, 'r') as read:
-        config = read.readlines()
-        with open(file, 'w') as write:
-            for line in config:
-                if line.strip().startswith(key):
-                    write.write(f'{key} = {value}\n')
-                    found = True
-                    print(f'Config found.\n Updating condif: {key} = {value}')
-                else:
-                    write.write(f'{line}')
-                    
+value = input("Enter the config value: ")
 
-            if not found:
-                print(f'No config found.\n Adding config: {key} = {value}')
-                write.write(f'\n{key} = {value}')
+def app_config(file,key,value):
+    found=False
+    with open(file,'r') as reader:
+        lines=reader.readlines()
+        with open(file, 'w') as writer:
+            for line in lines:
+                if line.strip().startswith(f'{key}'):
+                    writer.write(f'{key} = {value}\n')
+                    found=True
+                else:
+                    writer.write(line)
+        if not found:
+            writer.write(f'{key} = {value}\n')
 
 app_config(file,key,value)
+
+
+        
