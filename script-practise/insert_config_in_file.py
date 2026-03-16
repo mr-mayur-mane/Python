7fd4a91e916ff2e77b5e05e019a75e4128c05c33
mr-mayu-mane <mayurmane2577@gmail.com>
2026-03-16 18:50:17 +0530

    Add the new config to the file

--- script-practise/insert_config_in_file.py
+++ script-practise/insert_config_in_file.py
@@ -3,21 +3,21 @@ file = "app.conf"
 key = input("Enter the config key name: ")
 value = input("Enter the config value: ")
 
-def app_config(file,key,value):
-    found=False
-    with open(file,'r') as reader:
-        lines=reader.readlines()
+def app_config(file, key, value):
+    found = False
+    with open(file, 'r') as reader:
+        config = reader.readlines()
         with open(file, 'w') as writer:
-            for line in lines:
+            for line in config:
                 if line.strip().startswith(f'{key}'):
+                    print("Config key found, updating")
                     writer.write(f'{key} = {value}\n')
-                    found=True
+                    found = True
+                    print(f'Config updated: {key} = {value}')
                 else:
                     writer.write(line)
-        if not found:
-            writer.write(f'{key} = {value}\n')
+            if not found:
+                writer.write(f'{key} = {value}')
+                print("Config appending")
 
 app_config(file,key,value)
-
-
-        
