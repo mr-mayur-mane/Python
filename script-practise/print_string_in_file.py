48fb8351dabda7e6bdae107e1a9ac215eeae9722
mr-mayu-mane <mayurmane2577@gmail.com>
2026-03-25 18:27:49 +0530

    Print the keyword in file along with the line number

--- script-practise/print_string_in_file.py
+++ script-practise/print_string_in_file.py
@@ -1,16 +1,18 @@
-search = input("Enter the keyword to search: ").strip()
+search = input("Enter the keyword to be search: ").strip()
+
 file = "error.log"
 
 with open(file, 'r') as reader:
-    lines = reader.readlines()
+    lines= reader.readlines()
     found = False
     for line_no, line in enumerate(lines, start=1):
         if search in line.lower():
-            print()
-            print("Line number: ", line_no)
+            print(f'Line no: {line_no}')
             print(line)
             found = True
-
+    
     if not found:
-       print("No keyword found")
+        print(f'Keyword not found: {search}')
+
+
     
