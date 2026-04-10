72ac4dac765857a76299877b1d4db70b57721b80
mr-mayu-mane <mayurmane2577@gmail.com>
2026-04-10 18:49:33 +0530

    Create the script to list the file ending with some format in directory

--- script-practise/count_file_in_dir.py
+++ script-practise/count_file_in_dir.py
@@ -1,11 +1,23 @@
+# import os
+
+# dir = input("Enter the dir name: ")
+
+# if os.path.isdir(dir):
+#     print(f'Valid dir: {dir}')
+#     file = [ f for f in os.listdir(dir) if os.path.isfile(os.path.join(dir,f)) and f.endswith(".log")]
+#     print(f'File count in dir {dir}: {len(file)}')
+
+# else:
+#     print(f'Invalid dir path/ Dir not exits: {dir.strip()}')
+
 import os
 
-dir = input("Enter the dir name: ")
+dir = input("Enter the dir path: ")
 
 if os.path.isdir(dir):
-    print(f'Valid dir: {dir}')
-    file = [ f for f in os.listdir(dir) if os.path.isfile(os.path.join(dir,f)) and f.endswith(".log")]
-    print(f'File count in dir {dir}: {len(file)}')
+    file = [f for f in os.listdir(dir) if os.path.isfile(os.path.join(dir,f)) and f.endswith(".log")]
+
+    print(f'File ending with .log: {file}')
 
 else:
-    print(f'Invalid dir path/ Dir not exits: {dir.strip()}')
+    print(f'Invalid dir or dir not exists: {dir}')
