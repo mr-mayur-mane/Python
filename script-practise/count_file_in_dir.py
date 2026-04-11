73274b63eb80025915160c703a584fd191fa7f2a
mr-mayu-mane <mayurmane2577@gmail.com>
2026-04-11 21:37:48 +0530

    Removed the older script

--- script-practise/count_file_in_dir.py
+++ script-practise/count_file_in_dir.py
@@ -2,13 +2,7 @@
 
 # dir = input("Enter the dir name: ")
 
-# if os.path.isdir(dir):
-#     print(f'Valid dir: {dir}')
-#     file = [ f for f in os.listdir(dir) if os.path.isfile(os.path.join(dir,f)) and f.endswith(".log")]
-#     print(f'File count in dir {dir}: {len(file)}')
 
-# else:
-#     print(f'Invalid dir path/ Dir not exits: {dir.strip()}')
 
 import os
 
