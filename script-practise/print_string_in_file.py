959ff3413a0acce0d08159dda9777c9183807551
mr-mayu-mane <mayurmane2577@gmail.com>
2026-04-13 18:46:09 +0530

    Print the keyword in the file

--- script-practise/print_string_in_file.py
+++ script-practise/print_string_in_file.py
@@ -1,17 +1,3 @@
-# file = input("Enter the file name: ")
-# keyword = input("Enter the keyword to search in file: ")
-
-# with open(file, 'r') as f:
-#     found = False
-#     lines = f.readlines()
-#     for line in lines:
-#         if keyword in line:
-#             print(line)
-#             found = True
-        
-#     if not found:
-#         print(f'Keyword `{keyword}` not found in file `{file}`')
-
 search = input("Enter the keyword to search: ")
 
 file = "error.log"
