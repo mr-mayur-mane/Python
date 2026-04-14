3e4be8237a0733e28a981e2d07f66da7cceaa94e
mr-mayu-mane <mayurmane2577@gmail.com>
2026-04-14 19:07:15 +0530

    update the config in file

--- script-practise/insert_config_in_file.py
+++ script-practise/insert_config_in_file.py
@@ -1,27 +1,3 @@
-# file = "app.conf"
-
-# key = input("Enter the config key: ")
-# value= input("Enter hte config value: ")
-# def app_conf(file,key,value):
-#     found = False
-#     with open(file, 'r') as reader:
-#         config = reader.readlines()
-#         with open(file, 'w') as writer:
-#             for line in config:
-#                 if line.startswith(key):
-#                     print(f'Config found, updaing the value: \n{key} = {value}')
-#                     writer.write(f'{key} = {value}\n')
-#                     found = True
-#                 else:
-#                     writer.write(line)
-
-#             if not found:
-#                 print("Config not found, adding the config")
-#                 writer.write(f'\n{key}={value}')
-
-        
-# app_conf(file,key,value)
-
 file = "app.conf"
 
 config_key = input("Enter the config key: ")
