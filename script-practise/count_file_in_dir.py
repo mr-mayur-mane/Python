f637561c151572d76887e09b2eea73932e7296dd
mr-mayu-mane <mayurmane2577@gmail.com>
2026-05-07 17:26:28 +0530

    Search the string in the file

--- script-practise/count_file_in_dir.py
+++ script-practise/count_file_in_dir.py
@@ -1,15 +1,3 @@
-# import os
-
-# dir = input("Enter the dir path: ")
-
-# if os.path.isdir(dir):
-#     file = [f for f in os.listdir(dir) if os.path.isfile(os.path.join(dir,f)) and f.endswith(".log")]
-
-#     print(f'File ending with .log: {file}')
-
-# else:
-#     print(f'{dir}: Invalid dir or dir not exists')
-
 import os
 
 dir = input("Enter the directory path: ")
