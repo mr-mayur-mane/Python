50e44d55d068470c66d57bfaa90ad4ce212436f2
mr-mayu-mane <mayurmane2577@gmail.com>
2026-05-07 17:25:25 +0530

    Search the string in the file

--- script-practise/print_string_in_file.py
+++ script-practise/print_string_in_file.py
@@ -2,19 +2,6 @@ find = input("Enter the keyword to search: ")
 
 file = "error.log"
 
-# def search_keyword(search, file):
-#     found = False
-#     with open(file, 'r') as f:
-#         config=f.readlines()
-#         for line_no, line in enumerate(config, start=1):
-#             if search in line.lower():
-#                 print(f'Keyword Found: "{search}" found at {line_no}\n {line}')
-#                 found = True
-
-#         if not found:
-#             print(f'{search}: Not found')
-# search_keyword(search,file)
-
 def search_def(find, file):
     found = False
     with open(file, 'r') as f:
