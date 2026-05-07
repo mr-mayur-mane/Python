3edb9090d01d66b0cda4447b6149bfe81aba0c6c
mr-mayu-mane <mayurmane2577@gmail.com>
2026-05-07 16:57:13 +0530

    Print the file inside the dir ending with .log

--- script-practise/count_file_in_dir.py
+++ script-practise/count_file_in_dir.py
@@ -1,11 +1,21 @@
+# import os
+
+# dir = input("Enter the dir path: ")
+
+# if os.path.isdir(dir):
+#     file = [f for f in os.listdir(dir) if os.path.isfile(os.path.join(dir,f)) and f.endswith(".log")]
+
+#     print(f'File ending with .log: {file}')
+
+# else:
+#     print(f'{dir}: Invalid dir or dir not exists')
+
 import os
 
-dir = input("Enter the dir path: ")
+dir = input("Enter the directory path: ")
 
 if os.path.isdir(dir):
-    file = [f for f in os.listdir(dir) if os.path.isfile(os.path.join(dir,f)) and f.endswith(".log")]
-
-    print(f'File ending with .log: {file}')
-
+    file = [ f for f in os.listdir(dir) if os.path.isfile(os.path.join(dir,f)) and f.endswith(".log")]
+    print(f'Files ending with .log in dir {dir}: {file}')
 else:
-    print(f'{dir}: Invalid dir or dir not exists')
+    print(f'Directory not found: {dir}')
