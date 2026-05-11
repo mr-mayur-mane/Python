61903ae1ef39cf2ce8341d6c533a0ca6db2a29fb
mr-mayu-mane <mayurmane2577@gmail.com>
2026-05-11 10:19:17 +0530

    Added the count of the file

--- script-practise/count_file_in_dir.py
+++ script-practise/count_file_in_dir.py
@@ -5,5 +5,7 @@ dir = input("Enter the directory path: ")
 if os.path.isdir(dir):
     file = [ f for f in os.listdir(dir) if os.path.isfile(os.path.join(dir,f)) and f.endswith(".log")]
     print(f'Files ending with .log in dir {dir}: {file}')
+    print()
+    print("Count: ", len(file))
 else:
     print(f'Directory not found: {dir}')
